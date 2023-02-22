9c70ec56043831e78be69e356d7745656b9e698b
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-22 20:45:23 +0100

    fix server

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults({
   static: 'build',
   noCors: true
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 
--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' ?  '/api' : 'http://localhost:3000/api'
+export const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api'
